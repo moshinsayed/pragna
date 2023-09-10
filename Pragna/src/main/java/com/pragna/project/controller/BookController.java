87ae691210e50958951e986739cb0c426f2d132b
Moshin Sayed <moshin.sayed@yahoo.com>
2023-09-10 11:06:39 -0700

    First, test push.

--- Pragna/src/main/java/com/pragna/project/controller/BookController.java
+++ Pragna/src/main/java/com/pragna/project/controller/BookController.java
@@ -19,55 +19,55 @@ import com.pragna.project.service.BookService;
 public class BookController {
 
 	@Autowired
-	BookService bookservice;
+	BookService bookService;
 	
 	
 	@PostMapping("/add_new_book")
 	public ResponseEntity<?> addNewBook(@RequestParam("name") String bookName)
 	{
-		Long output=bookservice.addNewBook(bookName);
+		Long output= bookService.addNewBook(bookName);
 		return new ResponseEntity<Object>(output, HttpStatus.OK);
 	}
 	
 	@GetMapping("/get_all_books_with_pages")
 	public ResponseEntity<?> getAllBooksWithPages()
 	{
-		List<Map<String, Object>> output=bookservice.getAllBooksWithPages();
+		List<Map<String, Object>> output= bookService.getAllBooksWithPages();
 		return new ResponseEntity<Object>(output, HttpStatus.OK);
 	}
 	
 	@PostMapping("/add_new_page")
 	public ResponseEntity<?> addNewPage(@RequestParam("id") Long id, @RequestParam("pId") Long pId, @RequestParam("name") String pageName)
 	{
-		Long output=bookservice.addNewPage(id, pId, pageName);
+		Long output= bookService.addNewPage(id, pId, pageName);
 		return new ResponseEntity<Object>(output, HttpStatus.OK);
 	}
 	
 	@GetMapping("/get_max_page_id")
 	public ResponseEntity<?> getMaxPageId()
 	{
-		Long output=bookservice.getMaxPageId();
+		Long output= bookService.getMaxPageId();
 		return new ResponseEntity<Object>(output, HttpStatus.OK);
 	}
 	
 	@PostMapping("/update_page")
 	public ResponseEntity<?> updatePage(@RequestParam("id") Long id, @RequestParam("name") String pageName)
 	{
-		Long output=bookservice.updatePage(id, pageName);
+		Long output= bookService.updatePage(id, pageName);
 		return new ResponseEntity<Object>(output, HttpStatus.OK);
 	}
 	
 	@PostMapping("/delete_page")
 	public ResponseEntity<?> deletePage(@RequestParam("id") Long id)
 	{
-		Long output=bookservice.deletePage(id);
+		Long output= bookService.deletePage(id);
 		return new ResponseEntity<Object>(output, HttpStatus.OK);
 	}
 	
 	@PostMapping("/update_book")
 	public ResponseEntity<?> updateBook(@RequestParam("source_id") Long source_id, @RequestParam("target_id") Long target_id, @RequestParam("move") String move_type)
 	{
-		Long output=bookservice.updateBook(source_id, target_id, move_type);
+		Long output= bookService.updateBook(source_id, target_id, move_type);
 		return new ResponseEntity<Object>(output, HttpStatus.OK);
 	}
 }
